9bbf6283766040ca1ebf2974a706b9da85d8878c
aubur <aubyn@qq.com>
2025-08-24 13:14:56 +0800

    250823

--- assets/blog_input-method_custom_phrase.md.D5IEw359.js
+++ assets/blog_input-method_custom_phrase.md.D5IEw359.js
@@ -1,4 +1,4 @@
-import{l as a,c as n,b as p,ai as t}from"./chunks/framework.DfOqixww.js";const m=JSON.parse('{"title":"自定义短语","description":"","frontmatter":{},"headers":[],"relativePath":"blog/input-method/custom_phrase.md","filePath":"blog/input-method/custom_phrase.md"}'),l={name:"blog/input-method/custom_phrase.md"};function i(e,s,c,o,h,r){return p(),n("div",null,s[0]||(s[0]=[t(`<h1 id="自定义短语" tabindex="-1">自定义短语 <a class="header-anchor" href="#自定义短语" aria-label="Permalink to &quot;自定义短语&quot;">​</a></h1><p>补充个人常用的自定义短语，便于切换电脑时，可以快速复制使用。适用于 rime</p><div class="language-yaml vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">yaml</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># ~/.local/share/fcitx5/rime/dicts/custom_simple.dict.yaml</span></span>
+import{l as a,c as n,b as p,ai as t}from"./chunks/framework.DfOqixww.js";const m=JSON.parse('{"title":"自定义短语","description":"","frontmatter":{},"headers":[],"relativePath":"blog/input-method/custom_phrase.md","filePath":"blog/input-method/custom_phrase.md"}'),l={name:"blog/input-method/custom_phrase.md"};function e(i,s,c,o,h,r){return p(),n("div",null,s[0]||(s[0]=[t(`<h1 id="自定义短语" tabindex="-1">自定义短语 <a class="header-anchor" href="#自定义短语" aria-label="Permalink to &quot;自定义短语&quot;">​</a></h1><p>补充个人常用的自定义短语，便于切换电脑时，可以快速复制使用。适用于 rime</p><div class="language-yaml vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">yaml</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># ~/.local/share/fcitx5/rime/dicts/custom_simple.dict.yaml</span></span>
 <span class="line"><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">○	bt	100</span></span>
 <span class="line"><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">■	bt	100</span></span>
 <span class="line"><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">□	bt	100</span></span>
@@ -16,6 +16,8 @@ import{l as a,c as n,b as p,ai as t}from"./chunks/framework.DfOqixww.js";const m
 <span class="line"><span>❗	zy</span></span>
 <span class="line"><span>👍	zj</span></span>
 <span class="line"><span>➕	jw</span></span>
+<span class="line"><span>🔥	re</span></span>
+<span class="line"><span>🆕	xn</span></span>
 <span class="line"><span>➡️	yb</span></span>
 <span class="line"><span>1️⃣	y</span></span>
 <span class="line"><span>2️⃣	vr</span></span>
@@ -26,4 +28,4 @@ import{l as a,c as n,b as p,ai as t}from"./chunks/framework.DfOqixww.js";const m
 <span class="line"><span>7️⃣	qi</span></span>
 <span class="line"><span>8️⃣	ba</span></span>
 <span class="line"><span>9️⃣	jq</span></span>
-<span class="line"><span>🔟	si</span></span></code></pre></div>`,5)]))}const u=a(l,[["render",i]]);export{m as __pageData,u as default};
+<span class="line"><span>🔟	si</span></span></code></pre></div>`,5)]))}const u=a(l,[["render",e]]);export{m as __pageData,u as default};
