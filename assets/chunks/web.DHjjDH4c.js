4ecaef8bf375c6d8025bd8d5962b377d92a6619b
aubur <aubyn@qq.com>
2025-12-14 12:55:41 +0800

    251214

---
+++ assets/chunks/web.DHjjDH4c.js
@@ -0,0 +1,1 @@
+import{W as a}from"./theme.tqxI8GHW.js";import"./framework.B_cQq8f7.js";class i extends a{async show(t){if(typeof document<"u"){let o=2e3;t.duration&&(o=t.duration==="long"?3500:2e3);const e=document.createElement("pwa-toast");e.duration=o,e.message=t.text,document.body.appendChild(e)}}}export{i as ToastWeb};
